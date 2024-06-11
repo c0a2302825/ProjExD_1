fe84699e622f12f068262009dbea9ee1588a8ab1
Omoto <c0a2302825@edu.teu.ac.jp>
2024-06-11 16:49:27 +0900

    こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,20 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     pn_img = pg.image.load("fig/3.png")
     pn_img = pg.transform.flip(pn_img, True, False)
+    img_rct = pn_img.get_rect()
+    img_rct.center = 300, 200
     tmr = 0
+    x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(pn_img, [300, 200])
+        screen.blit(bg_img, [-(tmr%800), 0])
+        # screen.blit(pn_img, [0, 0])
+        screen.blit(pn_img, img_rct)
         pg.display.update()
-        tmr += 200        
-        clock.tick(10)
+        tmr += 1 
+        clock.tick(200)
 
 
 if __name__ == "__main__":
